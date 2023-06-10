8e76e208cd78086618e62ce7ee864685d71daced
maxbarriosm <maximilianobarriosmontero@Maximilianos-Laptop.local>
2023-06-10 16:45:59 -0400

    milestone 1

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,31 @@ class TweetsController < ApplicationController
         #mostrar los tweets de manera individual
         @tweet =Tweet.find params[:id]
     end
+
+    def new
+        @tweet = Tweet.new
+    end
+
+    def create
+        tweet = Tweet.new tweet_params
+        tweet.user = current_user
+        if tweet.save
+            redirect_to tweet, notice: "tweet guardado con exito"
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        tweet = Tweet.find params[:id]
+        tweet.destroy
+        redirect_to tweets_path, notice:  "Se ha eliminado correctamente"
+    end
+
+    private
+    def tweet_params
+        #le exigo que venga solamente un body y asi no tener vulnerabilidad
+        params.require(:tweet).permit(:body) 
+    end
+
 end
